3062eca0589d00429d029bf48d4abf4bbe010027
alekskakuk <alekskakuk@gmail.com>
2020-02-20 11:50:42 +0200

    Adding fake data in Vehicles
    In MVCAuto.Library
    Install-Package NBuilder
    Install-Package Faker.Net
    Adding random generator in seed method in Migrations->Configuration
    Add-Migration "Add_SeedOnly"
    Update-Database

--- MVCAuto.Library/Migrations/Configuration.cs
+++ MVCAuto.Library/Migrations/Configuration.cs
@@ -1,16 +1,22 @@
 ﻿namespace MVCAuto.Library.Migrations
 {
+    using FizzWare.NBuilder;
+    using MVCAuto.Library.DataAccess;
+    using MVCAuto.Library.Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<MVCAuto.Library.DataAccess.MVCAutoContext>
+
+    public sealed class Configuration : DbMigrationsConfiguration<MVCAuto.Library.DataAccess.MVCAutoContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
+
 
         protected override void Seed(MVCAuto.Library.DataAccess.MVCAutoContext context)
         {
@@ -18,6 +24,32 @@
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
+
+            ColorVehicleData dataColor = new ColorVehicleData();
+            var colorsVehicle = dataColor.GetColorVehiclesOrderByName().ToList();
+            var listColorId = (from d in colorsVehicle
+                                    select d.ColorId).ToList();
+
+
+            // Generate vehicles
+            var vinGenerator = new RandomGenerator();
+            var priceGenerator = new RandomGenerator();
+            var daysGenerator = new RandomGenerator();
+            var itemCountGenerator = new RandomGenerator();
+            var colorIdGenerator = new RandomGenerator();
+
+
+            var vehicles = Builder<Vehicle>.CreateListOfSize(1000)
+                .All()
+                    .With(v => v.Vin = vinGenerator.NextString(17,17))
+                    .With(v => v.Price = priceGenerator.Next(50, 500))
+                    .With(v => v.OperDate = DateTime.Now.AddDays(-daysGenerator.Next(1, 100)))
+                    .With(v => v.IntRowVer = itemCountGenerator.Next(1, 10))
+                    //.With(v => v. = Pick<ColorVehicle>.RandomItemFrom(colorsVehicle))
+                    .With(v => v.ColorId = Pick<int>.RandomItemFrom(listColorId))
+                    .Build();
+
+            context.Vehicles.AddOrUpdate(v => v.ID, vehicles.ToArray());
         }
     }
 }
